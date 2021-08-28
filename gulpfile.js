8ccc50f1c83181e8515cb99ae62124cae2307e88
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-08-29 01:03:33 +0600

    initial front-page

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const { src, dest, watch, series } = require('gulp');
+// const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
+
+// Sass Task
+function scssTask(){
+  return src('styles/scss/styles.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(dest('styles', { sourcemaps: '.' }));
+}
+
+// Watch Task
+function watchTask(){
+  watch('styles/scss/styles.scss', series(scssTask));
+}
+
+// Default Gulp task
+exports.default = series(
+  scssTask,
+  watchTask
+);
